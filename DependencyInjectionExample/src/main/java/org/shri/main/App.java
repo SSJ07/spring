0b60f6e35dc223f5169b0f2d450743589261de9c
shrikant jagtap <shrikantjagtap@gmail.com>
2018-01-02 00:23:14 +0530

    Depedency example in spring

--- DependencyInjectionExample/src/main/java/org/shri/main/App.java
+++ DependencyInjectionExample/src/main/java/org/shri/main/App.java
@@ -1,13 +1,27 @@
 package org.shri.main;
 
+import org.shri.models.Employee;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
- * Hello world!
+ * 
+ * SpringFramework supports three types of dependency injection.
+ * 1. Setter/Getter - used <property> tag
+ * 2. Constructor - <constructor-arg> tag
+ * 3. Interface
+ * 
+ * @author shri
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+    	Employee emp = (Employee) context.getBean("employee");
+    	System.out.println(emp);
+    	((ConfigurableApplicationContext)context).close();
     }
 }
